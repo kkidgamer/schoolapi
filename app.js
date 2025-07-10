33684dbb01765d206caf77cadf0226baeaec70bd
kkidgamer <khaembagift@gmail.com>
2025-07-10 09:37:40 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Entry file
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const e = require('cors');
+require('dotenv').config();
+
+// middlewares
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// static files accessibility
+app.use('/uploads', express.static('uploads'));
+
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log('Connected to MongoDB'))
+.catch((err) => console.log("MongoDB connection error:", err));
+
+const PORT=3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
