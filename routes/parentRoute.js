6489311178246510c8535d16b651c684ab77b01e
kkidgamer <129827158+kkidgamer@users.noreply.github.com>
2025-08-07 13:08:20 +0300

    Update parentRoute.js

--- routes/parentRoute.js
+++ routes/parentRoute.js
@@ -7,8 +7,9 @@ const { auth, authorizeRoles } = require('../middlware/auth');
 router.post('/', auth, authorizeRoles("admin"), parentController.addParent);
 router.get('/children', auth,authorizeRoles("parent"), parentDash.parentDash);
 router.get('/assignments/:id', auth, parentDash.getMyClassAssignments);
-router.get('/', auth, parentController.getAllParents);
 router.get('/:id', auth, parentController.getParentById);
+router.get('/', auth, parentController.getAllParents);
+
 router.put('/:id', auth, authorizeRoles("admin"), parentController.updateParent);
 router.delete('/:id', auth, authorizeRoles("admin"), parentController.deleteParent);
 module.exports=router
